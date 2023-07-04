f644b47b75e968db072b84f5744fe9326a99ba1d
alexandramikheyeva <alexandramikheeva@mail.ru>
2023-07-04 23:04:13 +0300

    1

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = local_settings.MEDIA_ROOT_local
+MEDIA_ROOT = local_settings.MEDIA_ROOT
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
