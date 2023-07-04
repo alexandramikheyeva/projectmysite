d61e816a9194e1031514e216c9398502c968eb64
alexandramikheyeva <alexandramikheeva@mail.ru>
2023-07-04 20:52:32 +0300

    1

--- src/proj/urls.py
+++ src/proj/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/', include('books.urls', namespace='books')),
+    path('bookshop/', include('bookshop.urls', namespace='bookshop')),
     path('', home_views.HomePage.as_view(), name= 'homepage'),
 ] 
 if settings.DEBUG:
