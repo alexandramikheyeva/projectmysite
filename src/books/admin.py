57672a2f300d5e612b30dca4a0f4ea2a354ba86d
alexandramikheyeva <alexandramikheeva@mail.ru>
2023-07-04 20:34:07 +0300

    new view

--- src/books/admin.py
+++ src/books/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
-admin.site.register(models.Autor)
+admin.site.register(models.Author)
 admin.site.register(models.Series)
 admin.site.register(models.Genre)
 admin.site.register(models.Publishing_House)
