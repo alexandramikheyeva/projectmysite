82b16db575ceed43369334df08776174a857b6bd
alexandramikheyeva <alexandramikheeva@mail.ru>
2023-07-05 22:23:47 +0300

    models

--- src/cart/admin.py
+++ src/cart/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+admin.site.register(models.BookInCart)
+admin.site.register(models.Cart)
+admin.site.register(models.Order)
