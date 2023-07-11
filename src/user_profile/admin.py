7e6ca65f5f5c3b65eded3d26302b39e0ee85edb0
alexandramikheyeva <alexandramikheeva@mail.ru>
2023-07-11 21:28:40 +0300

    1

--- src/user_profile/admin.py
+++ src/user_profile/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
+from .models import Account
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
 from . import models
 # Register your models here.
+
+
+
+class AccountInLine(admin.StackedInline):
+    model = Account
+    can_delete = False
+    verbose_name_plural = 'Account'
+
+
+class CustomizedUserAdmin(UserAdmin):
+    inlines = (AccountInLine, )
+
+
+admin.site.unregister(User)
+admin.site.register(models.User, CustomizedUserAdmin)
 admin.site.register(models.Address)
-admin.site.register(models.CustomerProfile)
