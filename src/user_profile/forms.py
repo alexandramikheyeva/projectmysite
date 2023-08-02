e34cc83594717ee005eae6d6b743dfdb0af5c424
alexandramikheyeva <alexandramikheeva@mail.ru>
2023-08-02 19:43:58 +0300

    a

---
+++ src/user_profile/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import CustomerProfile, Address
+from django.forms.models import modelform_factory
+
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        fields = ('country', 'city', 'postal_code', 'address_line_1', 'address_line_2')
+
+class CustomerProfileForm(forms.ModelForm):
+    class Meta:
+        model = CustomerProfile
+        fields = ('email_profile', 'first_profile_name', 'last_profile_name', 'phone', 'group', 'additional_info')
